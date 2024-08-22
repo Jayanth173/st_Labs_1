61cb74f40573f52b35676d81ed585c01547e0ae0
JAYANTH J R <93864425+Jayanth173@users.noreply.github.com>
2024-08-22 15:20:36 +0530

    changes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,4 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Progress } from './components/Progress.jsx'
